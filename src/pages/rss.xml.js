b83e0e1041c0d3571c1010b0e1fc0b712cd70f9a
=BjChacha <bjchacha@outlook.com>
2023-02-28 19:54:52 +0800

    init: my blog

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,9 +2,9 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function get() {
   return rss({
-    title: "Austin's Blog",
-    description: "Site description",
-    site: 'https://astro-blog.qum.cc',
+    title: "BjChacha's Blog",
+    description: "为自己记录，向别人分享",
+    site: 'http://120.24.187.189/',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>zh-cn</language>`,
   });
