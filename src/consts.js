5bdae30b1ae187772ec8b8d7ce4fc2ac7cdabfdb
=BjChacha <bjchacha@outlook.com>
2023-04-04 09:33:33 +0800

    reset project

--- src/consts.js
+++
@@ -1,6 +0,0 @@
-export const SITE_TITLE = `BjChacha's Blog`;
-export const SITE_DESCRIPTION = '为自己记录，向别人分享';
-export const SITE_EMAIL = 'bjchacha@outlook.com'
-// TODO: 暂时没有域名
-export const SITE_NAME = '120.24.187.189';
-export const SITE_URL = "https://astro-blog.qum.cc";
